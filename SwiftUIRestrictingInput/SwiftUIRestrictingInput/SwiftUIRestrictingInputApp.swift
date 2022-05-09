32411a14e6e9fd294267bbb74f75fdd4188a6d77
Darya Kuliashova <kuldiand@gmail.com>
2022-05-09 10:06:17 +0200

    Create project

---
+++ SwiftUIRestrictingInput/SwiftUIRestrictingInput/SwiftUIRestrictingInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRestrictingInputApp.swift
+//  SwiftUIRestrictingInput
+//
+//  Created by Darya Kuliashova on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRestrictingInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
