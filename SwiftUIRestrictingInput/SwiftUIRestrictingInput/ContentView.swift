ba33c7cea705440f07b69c5948a4e693a793b0a2
Darya Kuliashova <kuldiand@gmail.com>
2022-05-09 10:27:22 +0200

    Create views

--- SwiftUIRestrictingInput/SwiftUIRestrictingInput/ContentView.swift
+++ SwiftUIRestrictingInput/SwiftUIRestrictingInput/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var restrictWhitespaces = ""
+    @State private var restrictLetters = ""
+    @State private var restrictDecimals = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 48) {
+            Text("SWIFTUI")
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Doesn't accept whitespaces")
+                TextField("", text: $restrictWhitespaces)
+            }
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Doesn't accept letters")
+                TextField("", text: $restrictLetters)
+            }
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Doesn't accept decimals")
+                TextField("", text: $restrictDecimals)
+            }
+            Spacer()
+        }
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+        .padding(.horizontal, 24)
+        .padding(.top, 24)
     }
 }
 
